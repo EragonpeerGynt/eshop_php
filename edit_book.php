8b5bc2dc675bf9e6ae73d841ed0a9b00d2368658
EragonpeerGynt <eragon.peergynt@gmail.com>
2018-12-04 17:58:54 +0100

    Backup 012

--- edit_book.php
+++ edit_book.php
@@ -13,7 +13,7 @@ function overview($my_books) {
                 <?php foreach ($my_books as $tmp => $knjiga): ?>
                     <div class="book">
                         <form action="<?= $_SERVER["PHP_SELF"]?>" method="get">
-                            <input type="hidden" name="id" value="<?= $knjiga["id_book"] ?>" />
+                            <input type="hidden" name="id_edit" value="<?= $knjiga["id_book"] ?>" />
                             <p><?= $knjiga["author_name"] ?> : <?= $knjiga["title"] ?></p>
                             <p><?= number_format($knjiga["price"], 2) ?> EUR<br/>
                             <button type="submit">EDIT</button>
@@ -24,6 +24,33 @@ function overview($my_books) {
         </body>
     </html>
 <?php
+}
+
+function errorReport($error) {
+    ?>
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="styl.css">
+            <meta charset="UTF-8" />
+            <title>ERROR</title>
+        </head>
+        
+            <div id="login">
+                <div class="error">
+                    There seems to be an error
+                    <?php
+                    echo $error;
+                    ?>
+                    <br/>
+                </div>
+                <form action="./index.php" method="get">
+                    <button type="submit">Continue</button>
+                </form>
+            </div>
+             
+        </body>
+    </html>
+    <?php
 }
 
 function editbook() {
@@ -67,6 +94,17 @@ if (isset($_POST['editorial']) && $_POST['editorial'] == $_SESSION['user_id'] &&
         $books = DBBooks::getMyBooks($_SESSION['user_id']);
     }
     overview($books);
+}
+
+elseif (isset($_GET['id_edit'])) {
+    $book = DBBooks::getBook($_GET['id_edit'])[0];
+    if ($_SESSION['user_status'] == 'admin' || $book['id_seller'] == $_SESSION['user_id']) {
+        
+    }
+    else {
+        errorReport('<br/>You are not authorized to edit<br/>'.$book['title'].' by '.$book['author_name']);
+        exit();
+    }
 }
 
 /* 
